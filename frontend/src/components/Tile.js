5e3b3175a55adf7423c011bb682b0cfd0fbebbf1
Kimberly Heidorn <heidorkn@gmail.com>
2019-05-24 15:14:22 -0700

    adding mini game functionality

---
+++ frontend/src/components/Tile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Tile extends Component {
+
+  render(){
+    return(
+      <td>
+        <div>
+          $
+        </div>
+      </td>
+    )
+  }
+}
+
+export default Tile
--- frontend/src/containers/PunchABunch.js
+++ frontend/src/containers/PunchABunch.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react'
+import Tile from '../components/Tile'
+
+class GridTile {
+  constructor() {
+    this.Clicked = false;
+    this.value = 0
+  }
+}
 
 class PunchABunch extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      grid: []
+    }
+  }
+
   componentDidMount() {
-    
+    //number of columns
+    let grid = new Array (5)
+    for (let i = 0; i < 5; i++ ) {
+      //number of browser
+      grid[i] = new Array (10)
+      for (let j=0; j < 10; j++) {
+        grid[i][j] = new GridTile()
+      }
+    }
+    this.setState({ grid })
   }
 
   render() {
     return(
-      <div className="App-header">
-        <h1>Punch-A-Bunch</h1>
-        <div class="w3-row-padding">
-          <div class="w3-col l1 w3-center">
-          </div>
-        </div>
+      <div>
+        <table>
+        <tbody>
+          {this.state.grid.map((column, i) => {
+            return(
+              <tr key={ i }>
+                {column.map((row, j) => {
+                  return(
+                    <Tile key={ (i + "") + "," + (j + "") }/>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        </table>
       </div>
     )
   }
